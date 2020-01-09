6f8ad6067d2237d57d98c65711dd90e825b1d1d7
Ubuntu <aml-custom-module@aml-custom-module-vm2.naphbvfijznujpbmy5u3khmp5b.xx.internal.cloudapp.net>
2020-01-09 12:18:29 +0000

    support file input in convert to image dir

--- azureml-custom-module-examples/pytorch-modules/pytorchmodules/preprocess/convert_to_image_directory.py
+++ azureml-custom-module-examples/pytorch-modules/pytorchmodules/preprocess/convert_to_image_directory.py
@@ -4,8 +4,8 @@ from azureml.studio.core.logger import logger
 from azureml.studio.core.io.image_directory import ImageDirectory
 
 
-def entrance(input_path='/mnt/chjinche/test_data/classification/',
-             output_path='/mnt/chjinche/test_data/classification/image_dir/'):
+def entrance(input_path='/mnt/chjinche/test_data/classification/compressed/image_dataset.zip',
+             output_path='/mnt/chjinche/test_data/test/image_dir/'):
     logger.info('Start!')
     logger.info(f'input path {input_path}')
     # TODO:Case 1: input path is torchvision ImageFolder
@@ -13,10 +13,13 @@ def entrance(input_path='/mnt/chjinche/test_data/classification/',
     # Case 2: input path is compressed file
     compressed_extensions = {'.tar', '.zip'}
     compressed_path = None
-    for path in Path(input_path).glob(r'**/*'):
-        print(path)
-        if path.suffix in compressed_extensions:
-            compressed_path = path
+    if Path(input_path).is_file():
+        compressed_path = input_path
+    else:
+        for path in Path(input_path).glob(r'**/*'):
+            print(path)
+            if path.suffix in compressed_extensions:
+                compressed_path = path
 
     logger.info(f'compressed file path {compressed_path}')
     loader_dir = ImageDirectory.load_compressed(compressed_path)
